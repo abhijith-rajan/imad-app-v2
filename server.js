6241fffc78df123b134691c1c6f36d5045cba217
abhijith-rajan <rajanabhijith@gmail.com>
2017-02-19 13:13:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,6 +77,7 @@ function createTemplate(data)
                 </div>
             </body>
         </html>`;
+    return htmlTemplate;
 }           
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
