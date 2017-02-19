124663a46b145b0a1cffdd2cf9b2f431346017d3
abhijith-rajan <rajanabhijith@gmail.com>
2017-02-19 15:29:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,7 +19,7 @@ button.onclick=function(){
           if(request.status===200)
           {
               var counter=request.responseText;
-              alert(counter.toString());
+              console.log(counter.toString());
               var count =document.getElementById('count');
               count.innerHTML=counter.toString();
           }
