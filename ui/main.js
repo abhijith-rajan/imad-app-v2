7aeae51553365204b94a88523b2d0ec22dfd1c3b
abhijith-rajan <rajanabhijith@gmail.com>
2017-02-19 16:21:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -49,7 +49,7 @@ submit.onclick=function(){
           }
       }
     };
-    var nameInput=document.getElementByID("name");
+    var nameInput=document.getElementById("name");
     name=nameInput.value;
     request.open('GET','http://abhijith-rajan.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
